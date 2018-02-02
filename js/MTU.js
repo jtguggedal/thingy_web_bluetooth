7f5291b9eb9ca4d4603710cd2fe6b85d0cd21bd3
Thomas Skuggevik <thomas.skuggevik@gmail.com>
2018-02-02 13:41:27 +0100

    added MTU class for reading and writing maximal transmission unit

---
+++ js/MTU.js
@@ -0,0 +1,50 @@
+import Sensor from "./Sensor.js";
+
+class Mtu extends Sensor {
+  constructor(device, eventListeners = []) {
+    super(device, "mtu");
+
+    // gatt service and characteristic used to communicate with thingy's gas sensor
+    this.service = {
+      uuid: this.device.TCS_UUID,
+    };
+
+    this.characteristics = {
+      default: {
+        uuid: this.device.TCS_MTU_REQUEST_UUID,
+        decoder: this.decodeMtu.bind(this),
+        encoder: this.encodeMtu.bind(this),
+      }
+    };
+  }
+
+  decodeMtu(mtuSize) {
+    try {
+      const littleEndian = true;
+      const mtu = mtuSize.getUint16(1, littleEndian);
+
+      return mtu;
+    } catch (error) {
+      return new Error(`Error when decoding name data: ${error}`);
+    }
+  }
+
+  encodeMtu(mtuSize, peripheralRequest = false) {
+    try {
+      if (mtuSize < 23 || mtuSize > 276) {
+        return Promise.reject(new Error("MTU size must be in range 23 - 276 bytes"));
+      }
+
+      const dataArray = new Uint8Array(3);
+      dataArray[0] = peripheralRequest ? 1 : 0;
+      dataArray[1] = mtuSize & 0xff;
+      dataArray[2] = (mtuSize >> 8) & 0xff;
+
+      return dataArray;
+    } catch (error) {
+      return Promise.reject(new Error(`Error when encoding name data: ${error}`));
+    }
+  }
+}
+
+export default Mtu;
